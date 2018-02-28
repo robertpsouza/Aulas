5b440102d466828d98282d6e051a80f0734cf299
robertpsouza <robertpsouza@gmail.com>
2018-02-28 19:33:39 -0300

    n

---
+++ aula03/nota.php
@@ -0,0 +1,13 @@
+<?php
+echo "<pre>";
+
+
+$nota = 8;
+$frequencia = 9;
+$if ($nota >=7 and $frequencia >= 8){
+	echo "aprovado";
+} else if { $nota >= 5 && $nota <7  && $frequencia >=8);
+	echo "Recuperação";
+} else if { 
+
+
---
+++ aula03/op-ternario.php
@@ -0,0 +1,12 @@
+<?php
+echo "<pre>";
+$a = 3;
+$b = 2;
+if ($a > $b){
+	echo " A é maior que o B";
+} else {
+	echo " A não e maior que B";
+}
+echo "<hr>";
+
+echo ($a > $b) ? " A é maior que B" : "A não é amior que B";
