7fd53d7a22b203e0b833b681cd3f6de313ab13c6
robertpsouza <robertpsouza@gmail.com>
2018-02-27 21:20:18 -0300

    novo

---
+++ aula02/logicos.php
@@ -0,0 +1,15 @@
+<?php
+
+echo "<pre>";
+var_dump(true and true);
+var_dump(true and false);
+var_dump(true && true);
+var_dump(true && false);echo "<pre>";
+var_dump(true and true);
+var_dump(true and false);
+var_dump(true && true);
+var_dump(true && false);
+
+echo "<hr>";
+
+
---
+++ aula02/strings.php
@@ -0,0 +1,15 @@
+<?php
+echo "<pre>";
+$a = "1,2,3";
+echo "testando $a";
+echo "<br>";
+echo 'testando $a';
+echo "<hr>";
+echo "testando" .$a. "!!";
+echo "<hr>";
+echo "\"\ n";
+echo '\'';
+
+
+
+
