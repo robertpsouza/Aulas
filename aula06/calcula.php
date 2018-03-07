9060e55ab632a75e562d78ec4ac7921f76b1a1fc
robertpsouza <robertpsouza@gmail.com>
2018-03-06 22:22:10 -0300

    1

---
+++ aula06/calcula.php
@@ -0,0 +1,7 @@
+<?php
+
+$num1= $HTTP_GET_VARS["num1"];
+$num2 = $HTTP_GET_VARS["num2"];
+$op = $HTTP_GET_VARS["operacao"];
+
+if 
---
+++ aula07/teste.php
@@ -0,0 +1,9 @@
+<?php
+$host = "localhost";
+$user = "roberto";
+$pass = "@da4linux";
+$db= "aula07";
+
+$conexao = mysqli_connect ("localhost", "roberto", "@da4linux", "aula07");
+
+var_dump($conexao);
